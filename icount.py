b5b0f32005a10e33e1a7f4df379db80dce4d8e32
KostaGorod <korolx147@gmail.com>
2022-09-18 09:11:20 +0300

    add create_doc

--- icount.py
+++ icount.py
@@ -5,10 +5,11 @@ https://www.icount.co.il/api-v3/
 import requests
 
 
-def post(url, data):
-    req = requests.post(url=url, params=data)
+def post(url, data=None, json=None, params=None):
+    req = requests.post(url=url, data=data, json=json, params=params)
     resp_json = req.json()
     if not resp_json.get("status", "False"):
+        print(resp_json)
         raise Exception(
             resp_json.get(
                 "error_description", "General Error, no error_description found"
@@ -35,24 +36,24 @@ class iCountSession(object):
     def login(self, company, username, password):
         self.logout()
         endpoint = "https://api.icount.co.il/api/v3.php/auth/login"
-        self.sid = post(endpoint, {"user": username, "cid": company, "pass": password})[
-            "sid"
-        ]
+        self.sid = post(
+            endpoint, params={"user": username, "cid": company, "pass": password}
+        )["sid"]
 
     def logout(self):
         if not self.sid:
             return
         endpoint = "https://api.icount.co.il/api/v3.php/auth/logout"
-        post(endpoint, {"sid": self.sid})
+        post(endpoint, params={"sid": self.sid})
         self.sid = None
 
     def supplier_list(self):
         endpoint = "https://api.icount.co.il/api/v3.php/supplier/get_list"
-        res = post(endpoint, {"sid": self.sid, "detail_level": 10})
+        res = post(endpoint, params={"sid": self.sid, "detail_level": 10})
         return res.get("suppliers")
 
     def create_doc(self, doc: dict):
         endpoint = "https://api.icount.co.il/api/v3.php/doc/create"
-        res = post(endpoint, {"sid": self.sid} | doc)
+        res = post(endpoint, json={"sid": self.sid} | doc)
         return res
 
